b3055c9555d3f37f9ffd9b2bb054de6d1f42a454
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2022-11-16 22:16:44 +0530

    signup form data

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  styleUrls: ['./signup.component.scss'],
 })
 export class SignupComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  signupFormData(data: any) {
+    console.log(data);
   }
-
 }
