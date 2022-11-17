8bb65f8fb59c3496426e186fe5659485fbadf814
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2022-11-17 12:51:30 +0530

    crud operation is done

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -10,6 +10,9 @@ import { EmployeeModel } from './dashboard.model';
 })
 export class DashboardComponent implements OnInit {
   formValue!: FormGroup;
+  employeeData!: any;
+  showAddBtn!: boolean;
+  showUpdateBtn!: boolean;
   employeeModelObject: EmployeeModel = new EmployeeModel();
   constructor(private formbuilder: FormBuilder, private api: ApiService) {}
 
@@ -21,8 +24,11 @@ export class DashboardComponent implements OnInit {
       mobile: [''],
       salary: [''],
     });
+
+    this.getAllEmployee();
   }
 
+  // add new data
   postEmployeeDetails() {
     this.employeeModelObject.firstName = this.formValue.value.firstName;
     this.employeeModelObject.lastName = this.formValue.value.lastName;
@@ -34,10 +40,80 @@ export class DashboardComponent implements OnInit {
       (res) => {
         console.log(res);
         alert('Emp added successfully');
+
+        // close the modal after form are add
+        let ref = document.getElementById('cancel');
+        ref?.click();
+
+        // reset form field
+        this.formValue.reset();
+
+        // new data is update in DOM
+        this.getAllEmployee();
       },
       (err) => {
         alert(err + 'Something want wrong');
       }
     );
+  }
+
+  // get all data
+  getAllEmployee() {
+    this.api.getEmployee(this.employeeModelObject).subscribe((res) => {
+      this.employeeData = res;
+    });
+  }
+
+  // delete existing data from list
+  deleteEmployee(item: any) {
+    this.api.deleteEmployee(item.id).subscribe((res) => {
+      alert('Employee Data is deleted');
+
+      // when data is deleted DOM will be update automatically
+      this.getAllEmployee();
+    });
+  }
+
+  // set existing data when i click edit btn
+  onEdit(item: any) {
+    this.showAddBtn = false;
+    this.showUpdateBtn = true;
+    this.employeeModelObject.id = item.id;
+    this.formValue.controls['firstName'].setValue(item.firstName);
+    this.formValue.controls['lastName'].setValue(item.lastName);
+    this.formValue.controls['email'].setValue(item.email);
+    this.formValue.controls['mobile'].setValue(item.mobile);
+    this.formValue.controls['salary'].setValue(item.salary);
+  }
+
+  // update the data
+  updateEmployeeDetails() {
+    this.employeeModelObject.firstName = this.formValue.value.firstName;
+    this.employeeModelObject.lastName = this.formValue.value.lastName;
+    this.employeeModelObject.email = this.formValue.value.email;
+    this.employeeModelObject.mobile = this.formValue.value.mobile;
+    this.employeeModelObject.salary = this.formValue.value.salary;
+
+    this.api
+      .updateEmployee(this.employeeModelObject, this.employeeModelObject.id)
+      .subscribe((res) => {
+        alert('Data will be updated');
+        // close the modal after form are add
+        let ref = document.getElementById('cancel');
+        ref?.click();
+
+        // reset form field
+        this.formValue.reset();
+
+        // new data is update in DOM
+        this.getAllEmployee();
+      });
+  }
+
+  // remove update btn or add btn
+  clickAddEmployee() {
+    this.formValue.reset();
+    this.showAddBtn = true;
+    this.showUpdateBtn = false;
   }
 }
