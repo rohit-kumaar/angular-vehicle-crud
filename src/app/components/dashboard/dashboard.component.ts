10f6820ff57c12ccfcaa4b8be00c6e9d6fe75a23
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2022-11-17 12:04:15 +0530

    modal post data is working

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { ApiService } from 'src/app/service/api.service';
+import { EmployeeModel } from './dashboard.model';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,7 +10,8 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class DashboardComponent implements OnInit {
   formValue!: FormGroup;
-  constructor(private formbuilder: FormBuilder) {}
+  employeeModelObject: EmployeeModel = new EmployeeModel();
+  constructor(private formbuilder: FormBuilder, private api: ApiService) {}
 
   ngOnInit(): void {
     this.formValue = this.formbuilder.group({
@@ -18,5 +21,23 @@ export class DashboardComponent implements OnInit {
       mobile: [''],
       salary: [''],
     });
+  }
+
+  postEmployeeDetails() {
+    this.employeeModelObject.firstName = this.formValue.value.firstName;
+    this.employeeModelObject.lastName = this.formValue.value.lastName;
+    this.employeeModelObject.email = this.formValue.value.email;
+    this.employeeModelObject.mobile = this.formValue.value.mobile;
+    this.employeeModelObject.salary = this.formValue.value.salary;
+
+    this.api.postEmployee(this.employeeModelObject).subscribe(
+      (res) => {
+        console.log(res);
+        alert('Emp added successfully');
+      },
+      (err) => {
+        alert(err + 'Something want wrong');
+      }
+    );
   }
 }
